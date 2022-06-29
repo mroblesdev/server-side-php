19d48d8ce2823e22207585e4bdfa8e9fc945bc01
Marco Robles <markorobles75@gmail.com>
2022-06-29 00:27:26 -0500

    Add Bootstrap v5.1.3

--- index.php
+++ index.php
@@ -9,48 +9,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Almacen</title>
 
-    <style>
-        table,
-        th,
-        td {
-            border: 1px solid;
-        }
-
-        table {
-            width: 80%;
-            border-collapse: collapse;
-        }
-    </style>
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 </head>
 
 <body>
+    <main>
+        <div class="container py-4 text-center">
+            <h2>Empleados</h2>
 
-    <h2>Empleados</h2>
+            <div class="row justify-content-md-center">
+                <div class="col-md-4">
+                    <form action="" method="post">
+                        <label for="campo">Buscar: </label>
+                        <input type="text" name="campo" id="campo">
+                    </form>
+                </div>
+            </div>
 
-    <form action="" method="post">
-        <label for="campo">Buscar: </label>
-        <input type="text" name="campo" id="campo">
-    </form>
+            <div class="row py-4">
+                <div class="col">
+                    <table class="table table-sm table-bordered">
+                        <thead>
+                            <th>Num. empleado</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Fecha nacimiento</th>
+                            <th>Fecha ingreso</th>
+                            <th></th>
+                            <th></th>
+                        </thead>
 
-    <p></p>
+                        <!-- El id del cuerpo de la tabla. -->
+                        <tbody id="content">
 
-    <table>
-        <thead>
-            <th>Num. empleado</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Fecha nacimiento</th>
-            <th>Fecha ingreso</th>
-            <th></th>
-            <th></th>
-        </thead>
-
-        <!-- El id del cuerpo de la tabla. -->
-        <tbody id="content">
-
-        </tbody>
-    </table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </main>
 
     <script>
         /* Llamando a la función getData() */
@@ -76,6 +75,9 @@
                 }).catch(err => console.log(err))
         }
     </script>
+
+    <!-- Bootstrap core JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
 
